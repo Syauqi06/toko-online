c0ffd40bfc865059cb1a365c99798ad923e04dea
Syauqi06 <syauqi0066@gmail.com>
2025-10-27 10:29:41 +0700

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        User::create([
+            'nama' => 'Administrator',
+            'email' => 'admin@gmail.com',
+            'role' => '1',
+            'status' => 1,
+            'hp' => '0812345678901',
+            'password' => bcrypt('P@55word'),
+        ]);
+        
+        User::create([
+            'nama' => 'Sopian Aji',
+            'email' => 'sopian4ji@gmail.com',
+            'role' => '0',
+            'status' => 1,
+            'hp' => '081234567892',
+            'password' => bcrypt('P@55word'),
+        ]);
+
+        User::create([
+            'nama' => 'Syauqi',
+            'email' => 'syauqi8787@gmail.com',
+            'role' => '1',
+            'status' => 1,
+            'hp' => '086542738492',
+            'password' => bcrypt('syauqi8787'),
+        ]);
+    }
+}
